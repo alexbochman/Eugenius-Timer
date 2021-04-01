5bb2c39f605a38e212b83a68fb8c5d9407e51621
Alex Bochman <alexbochman22@gmail.com>
2021-04-01 15:39:06 -0400

    Refactoring, form progress

--- UI.js
+++ UI.js
@@ -5,10 +5,10 @@ var toggleState = "open";
 
 function toggleNav() {
 
-    if(toggleState == "open") {
+    if (toggleState == "open") {
         closeNav();
     }
-    else if(toggleState == "closed") {
+    else if (toggleState == "closed") {
         openNav();
 
     }
@@ -17,18 +17,18 @@ function toggleNav() {
 // SET THE PROPERTIES AND STYLES WHEN THE NAV BAR OPENS
 function openNav() {
     toggleState = "open";
-    
+
     var img = document.getElementById("toggleIcon");
     img.setAttribute("class", "fas fa-angle-double-left");
 
     var extLinks = document.getElementsByClassName("fas fa-external-link-alt");
-    for(var i = 0; i < extLinks.length; i++)
+    for (var i = 0; i < extLinks.length; i++)
         extLinks[i].setAttribute("class", "fas fa-external-link-alt");
-    
-    
+
+
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
-    document.getElementById("sideNavHeader").style.visibility = "visible"; 
+    document.getElementById("sideNavHeader").style.visibility = "visible";
     var x = document.getElementById("navList");
     var y = x.getElementsByTagName("A");
     var i;
@@ -40,17 +40,17 @@ function openNav() {
 // SET THE PROPERTIES AND STYLES WHEN THE NAV BAR CLOSES
 function closeNav() {
     toggleState = "closed";
-    
+
     var img = document.getElementById("toggleIcon");
     img.setAttribute("class", "fas fa-angle-double-left rotated-icon");
 
     var extLinks = document.getElementsByClassName("fas fa-external-link-alt");
-    for(var i = 0; i < extLinks.length; i++)
+    for (var i = 0; i < extLinks.length; i++)
         extLinks[i].setAttribute("class", "fas fa-external-link-alt hide");
-    
+
     document.getElementById("mySidenav").style.width = "50px";
     document.getElementById("main").style.setProperty("margin-left", "50px", "important");
-    document.getElementById("sideNavHeader").style.visibility = "hidden"; 
+    document.getElementById("sideNavHeader").style.visibility = "hidden";
 
     var x = document.getElementById("navList");
     var y = x.getElementsByTagName("A");
@@ -72,7 +72,7 @@ function closeNav() {
     for (i = 0; i < y.length; i++) {
         y[i].style.visibility = "visible";
     }
-    
+
     var x = document.getElementById("navList");
     var y = x.getElementsByClassName("far");
     var i;
@@ -85,23 +85,22 @@ function activeTab(id) {
     var divid = document.getElementById(id);
     var divs = document.getElementsByClassName("tab");
 
-    for(var i=0;i<divs.length;i++) {
+    for (var i = 0; i < divs.length; i++) {
         divs[i].style.backgroundColor = "#34527d";
     }
-    
+
     divid.style.backgroundColor = "#44628d";
 }
 
-function newTab() { 
-    window.open( 
-        "https://github.com/alexbochman/VS-Saturn-3", "_blank"); 
-} 
+function newTab() {
+    window.open(
+        "https://github.com/alexbochman/VS-Saturn-3", "_blank");
+}
 
 // FUNCTIONS BELOW SHOWS THE CONTENT OF THE TAB WHEN CLICKED IN THE NAV BAR
 // AND HIDES ALL OTHER CONTENT
 function mediaToggle() {
     if (x.matches) { // If media query matches
-
         closeNav();
     } else {
         openNav();
@@ -109,19 +108,19 @@ function mediaToggle() {
 }
 
 var x = window.matchMedia("(max-width: 800px)");
-mediaToggle(x); // Call listener function at run time
+//mediaToggle(x); // Call listener function at run time
 x.addListener(mediaToggle); // Attach listener function on state changes
 
 
-function showhide(id){
+function showhide(id) {
     if (document.getElementById(id)) {
         var divid = document.getElementById(id);
         var divs = document.getElementsByClassName("myHide");
-        for(var i=0;i<divs.length;i++) {
+        for (var i = 0; i < divs.length; i++) {
             divs[i].style.display = "none";
         }
         divid.style.display = "block";
-    } 
+    }
     return false;
 }
 
@@ -131,17 +130,8 @@ function showhide(id){
 function rotateChevron(id) {
     var chevron = document.getElementById(id);
 
-    if(chevron.classList.contains("rotatedChevron")) 
+    if (chevron.classList.contains("rotatedChevron"))
         chevron.setAttribute("class", "fas fa-chevron-right");
     else
         chevron.setAttribute("class", "fas fa-chevron-right rotatedChevron");
 }
-
-
-
-
-
-
-
-
-
