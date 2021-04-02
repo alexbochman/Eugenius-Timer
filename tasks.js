3499e715d81aac4970874dbbb9db88ad6089972f
Alex Bochman <alexbochman22@gmail.com>
2021-04-02 12:40:05 -0400

    refactored deleteTask()

--- tasks.js
+++ tasks.js
@@ -1,14 +1,14 @@
-taskList = [];
-taskListSize = taskList.length;
+var taskList = [];
+var taskListSize = taskList.length;
 
 //total number of subFields created, including deleted ones
-subFieldNum = 0;
+var subFieldNum = 0;
 
 function addSubField(id){
     var test = document.getElementById(id);
     var newEl = document.createElement("div");
     newEl.setAttribute("class", "col-xs-12");
-    
+
     newEl.innerHTML =   "<div class='col-xs-6'>" +
                             "<label for='subName" + subFieldNum + "'>Sub Task Name</label>" +
                             "<input type='text' id='subName" + subFieldNum + "'" +
@@ -22,8 +22,8 @@ function addSubField(id){
                         "</div>" +
 
                         "<button onclick='removeSubField(this)' class='col-xs-12 btn-danger' style='margin-bottom: 10px; margin-top: -20px;'>Remove Sub Task</button>";
-                        
-    
+
+
     newEl.style.border = "1px solid #ccc";
     newEl.style.borderRadius = "4px";
     test.append(newEl);
@@ -48,7 +48,7 @@ function completeTaskCreation(submit){
     }
     var newTask = createTask(name, desc, subTasksString);
     moveToUncomplete(newTask);
-    
+
     //now that the task is completed and set in the right place we need to empty the form.
     submit.children[0].children[2].value = "New Task Title";
     submit.children[1].children[2].innerHTML = "Optionally provide a meaningful description.";
@@ -59,7 +59,7 @@ function completeTaskCreation(submit){
     taskList.push({title:name, desc:desc, subTasks:subTaskObjects});
     // localStorage.setItem("taskList", JSON.stringify(taskList));
     // localStorage.setItem("taskListSize", taskList.length);
-    
+
 }
 
 //the buttons need to be setup after the fact because the onclick needs to be changed, it's a pain but needs to be done this way
@@ -78,8 +78,8 @@ function createTask(name, desc, subTasksString){
                         "</div>" +
                         "<div id='task" + taskListSize + "' class='collapse col-xs-12'>" +
                             "<p>" + desc + "</p>" +
-                            "<ul>" + 
-                                subTasksString + 
+                            "<ul>" +
+                                subTasksString +
                             "</ul>" +
                         "</div>";
     return newEl;
@@ -97,21 +97,152 @@ function moveToUncomplete(objectToMove){
                                                      "<button type='button' class='btn-danger' onclick='deleteTask(this.parentElement.parentElement.parentElement, \"taskList\")'>Delete</button>";
 }
 
-function deleteTask(objectToDelete, listid){
-    var list = document.getElementById(listid);
-    var newList = [];
-    var notFound = true;
-    var i;
-    for(i = 0; i < taskList.length; i++){
-        if(list.children[i] === objectToDelete){
-            notFound = false;
-            objectToDelete.remove();
-        } else {
-            newList.push(taskList[i]);
-            console.log(newList.length);
-        }
+var taskList = [];
+var taskListSize = taskList.length;
+
+//total number of subFields created, including deleted ones
+var subFieldNum = 0;
+
+function addSubField(id) {
+  var test = document.getElementById(id);
+  var newEl = document.createElement("div");
+  newEl.setAttribute("class", "col-xs-12");
+
+  newEl.innerHTML =
+    "<div class='col-xs-6'>" +
+    "<label for='subName" +
+    subFieldNum +
+    "'>Sub Task Name</label>" +
+    "<input type='text' id='subName" +
+    subFieldNum +
+    "'" +
+    "value='Sub Task Name'>" +
+    "</div>" +
+    "<div class='col-xs-6'>" +
+    "<label for='subDesc" +
+    subFieldNum +
+    "'>Sub Task Description</label>" +
+    "<input type='text' id='subDesc" +
+    subFieldNum +
+    "'" +
+    "value='Sub Task Description'>" +
+    "</div>" +
+    "<button onclick='removeSubField(this)' class='col-xs-12 btn-danger' style='margin-bottom: 10px; margin-top: -20px;'>Remove Sub Task</button>";
+
+  newEl.style.border = "1px solid #ccc";
+  newEl.style.borderRadius = "4px";
+  test.append(newEl);
+  subFieldNum++;
+}
+
+function removeSubField(elementToRemove) {
+  var parent = elementToRemove.parentElement;
+  parent.remove();
+}
+
+function completeTaskCreation(submit) {
+  var childElements = submit.children;
+  var name = childElements[0].children[2].value;
+  var desc = childElements[1].children[2].value;
+  var subTaskObjects = [];
+  var subTasks = childElements[2].children[2].children;
+  var subTasksString = "";
+  for (var i = 0; i < subTasks.length; i++) {
+    subTaskObjects.push({
+      name: subTasks[i].children[0].children[1].value,
+      desc: subTasks[i].children[1].children[1].value,
+    });
+    subTasksString +=
+      "<div><p class='col-xs-4'>" +
+      subTaskObjects[i].name +
+      "</p><p class='col-xs-8'>" +
+      subTaskObjects[i].desc +
+      "</p></div>";
+  }
+  var newTask = createTask(name, desc, subTasksString);
+  moveToUncomplete(newTask);
+
+  //now that the task is completed and set in the right place we need to empty the form.
+  submit.children[0].children[2].value = "New Task Title";
+  submit.children[1].children[2].innerHTML =
+    "Optionally provide a meaningful description.";
+  subFieldNum = 0;
+  submit.children[2].children[2].innerHTML = "";
+
+  taskListSize++;
+  taskList.push({ title: name, desc: desc, subTasks: subTaskObjects });
+  // localStorage.setItem("taskList", JSON.stringify(taskList));
+  // localStorage.setItem("taskListSize", taskList.length);
+}
+
+//the buttons need to be setup after the fact because the onclick needs to be changed, it's a pain but needs to be done this way
+function createTask(name, desc, subTasksString) {
+  var newEl = document.createElement("div");
+  newEl.setAttribute("class", "task");
+  newEl.innerHTML =
+    "<div class='topRow col-xs-12'>" +
+    "<button type='button' " +
+    "class='btn btn-info' " +
+    "data-toggle='collapse' " +
+    "data-target='#task" +
+    taskListSize +
+    "' onClick=rotateChevron('task" +
+    taskListSize +
+    "Chevron')>" +
+    "<h4 class='taskTitle'>" +
+    name +
+    " &nbsp<span><i class='fas fa-chevron-right' id='task" +
+    taskListSize +
+    "Chevron'></i></span></h4>" +
+    "</button>" +
+    "<div class='taskOptions'>" +
+    "</div>" +
+    "</div>" +
+    "<div id='task" +
+    taskListSize +
+    "' class='collapse col-xs-12'>" +
+    "<p>" +
+    desc +
+    "</p>" +
+    "<ul>" +
+    subTasksString +
+    "</ul>" +
+    "</div>";
+  return newEl;
+}
+
+function moveToComplete(objectToMove) {
+  document.getElementById("completedList").append(objectToMove);
+  objectToMove.children[0].children[1].innerHTML =
+    "<button type='button' class='btn-success' onclick='moveToUncomplete(this.parentElement.parentElement.parentElement)'>Uncomplete</button>" +
+    "<button type='button' class='btn-danger' onclick='deleteTask(this.parentElement.parentElement.parentElement, \"completedList\")'>Delete</button>";
+}
+
+function moveToUncomplete(objectToMove) {
+  document.getElementById("taskList").append(objectToMove);
+  objectToMove.children[0].children[1].innerHTML =
+    "<button type='button' class='btn-success' onclick='moveToComplete(this.parentElement.parentElement.parentElement)'>Complete</button>" +
+    "<button type='button' class='btn-danger' onclick='deleteTask(this.parentElement.parentElement.parentElement, \"taskList\")'>Delete</button>";
+}
+
+function deleteTask(objectToDelete, listid) {
+  var list = document.getElementById(listid);
+  var newList = [];
+  var found = false;
+  var i;
+  for (i = 0; i < taskList.length; i++) {
+    if (list.children[i] === objectToDelete) {
+      found = true;
+      objectToDelete.remove();
+    } else {
+      newList.push(taskList[i]);
     }
-    if(notFound)
-        console.log("error: the task you selected wasn't found");
-    taskList = newList;
+  }
+
+  //console.log("newlist.length = " + newList.length);
+
+  if (!found) console.log("error: the task you selected wasn't found");
+  taskList = newList;
+
+  console.log("taskList.length = " + taskList.length);
 }
